e647dbd58840baeca3f5479755d42adef9dd4297
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-06-17 10:47:29 +0100

    Update admin.py

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# This adds models to the admin site
+# for this run python manage.py createsuperuser to make a login to thee site
 
 from .models import Question, Choice
 
